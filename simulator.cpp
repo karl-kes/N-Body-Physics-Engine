a7347603cb2a39194d3730d6c37bfe0c974ce49d
Karl Keshavarzi <kkeshavarzi@outlook.com>
2025-11-12 12:18:02 -0500

    Implemented CSV files for the configuration of bodies; working on the same for the simulation settings.

--- simulator.cpp
+++ simulator.cpp
@@ -3,11 +3,15 @@
 #include <cmath>
 #include <iomanip>
 #include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 static constexpr double G{ 6.67e-11 };
+static constexpr double CONVERT_TO_KMH{ 3.6 };
+static constexpr double CONVERT_TO_KM{ 1000.0 };
 
-class Vec_3D {
-public:
+struct Vec_3D {
     double x_, y_, z_;
     double norm() {
         return std::sqrt( x_*x_ + y_*y_ + z_*z_ );
@@ -70,18 +74,54 @@ public:
     double get_mass() const { return mass_; }
 };
 
+// Load bodies.
+void load_csv_bodies( std::string file_name, std::vector<Body> &bodies ) {
+    std::ifstream file( file_name );
+    if ( !file.is_open() ) {
+        std::cout << "Error opening " << file_name << ".csv." << std::endl;
+        return;
+    }
+
+    std::string line{};
+    while ( std::getline( file, line ) ) {
+        if ( line.empty() || line[0] == '#' ) continue;
+
+        std::stringstream string_stream( line );
+        std::string segment{};
+        std::vector<double> values{};
+
+        while ( std::getline( string_stream, segment, ',' ) ) {
+            values.push_back( std::stod( segment ) );
+        }
+
+        if ( values.size() == 7 ) {
+            bodies.push_back( { { values[0], values[1], values[2] }, { values[3], values[4], values[5] }, values[6] } );
+        }
+    }
+    file.close();
+}
+
+// Load the dt and number of steps.
+void load_simulation_settings( std::string file_name, double &dt, int &steps ) {
+    std::ifstream file( file_name );
+    if ( !file.is_open() ) {
+        std::cout << "Error opening " << file_name << ".csv." << std::endl;
+        return;
+    }
+
+    std::string line{};
+    while ( std::getline( file, line ) ) { 
+        if ( line.empty() || line[0] == '#' ) continue;
+    }
+}
+
 int main() {
-    constexpr double MASS{ 1.0e20 };
-    constexpr double POS{ 1.0e4 };
-    constexpr double dt{ 0.1 };
-    int steps{ 0 };
-    int current_step{ 0 };
+    double dt{0.1};
+    int steps{};
+    int current_step{};
 
-    Body B_1{ { -1.0*POS, POS, POS }, { 0, 0, 0 }, MASS };
-    Body B_2{ { POS, -1.0*POS, POS }, { 0, 0, 0 }, MASS };
-    Body B_3{ { POS, POS, -1.0*POS }, { 0, 0, 0 }, MASS };
-
-    std::vector<Body> bodies{ B_1, B_2, B_3 };
+    std::vector<Body> bodies{};
+    load_csv_bodies( "bodies.csv", bodies );
 
     std::cout << "<--- 3-Body Simulation --->" << std::endl;
 
@@ -109,14 +149,14 @@ int main() {
             Vec_3D curr_body_pos{ bodies[idx].get_pos() };
             Vec_3D curr_body_vel{ bodies[idx].get_vel() };
 
-            std::cout << "Body " << idx << ": Pos(" << curr_body_pos.x_ / 1000.0 << ", " 
-                                                    << curr_body_pos.y_ / 1000.0 << ", " 
-                                                    << curr_body_pos.z_ / 1000.0 << ") km, ";
+            std::cout << "Body " << idx + 1 << ": Pos(" << curr_body_pos.x_ / CONVERT_TO_KM << ", " 
+                                                    << curr_body_pos.y_ / CONVERT_TO_KM << ", " 
+                                                    << curr_body_pos.z_ / CONVERT_TO_KM << ") km, ";
 
-            std::cout << "Vel(" << curr_body_vel.x_ / 3.6 << ", " 
-                                                    << curr_body_vel.y_ / 3.6 << ", " 
-                                                    << curr_body_vel.z_ / 3.6 << ") km/h, ";
-            std::cout << "Speed: " << curr_body_vel.norm() / 3.6 << " km/h" << std::endl;
+            std::cout << "Vel(" << curr_body_vel.x_ * CONVERT_TO_KMH << ", " 
+                                << curr_body_vel.y_ * CONVERT_TO_KMH << ", " 
+                                << curr_body_vel.z_ * CONVERT_TO_KMH << ") km/h, ";
+            std::cout << "Speed: " << curr_body_vel.norm() * CONVERT_TO_KMH << " km/h" << std::endl;
         }
         std::cout << std::endl;
 
@@ -128,7 +168,7 @@ int main() {
                 Vec_3D R{ bodies[idx].get_pos() - bodies[second_idx].get_pos() };
                 std::cout << "Distance: B" << idx + 1 << "-B"
                                            << second_idx + 1 << ": "
-                                           << R.norm() / 1000.0 << " km" << std::endl;
+                                           << R.norm() / CONVERT_TO_KM << " km" << std::endl;
             }
             std::cout << std::endl;
         }
